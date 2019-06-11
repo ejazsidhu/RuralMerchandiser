339533a8e0bdb8c64a18b1f19c4d49f17ebcadbd
ejazsidhu <ejazsidhu@gmail.com>
2019-06-11 13:14:54 +0500

    section 2 is populated

--- src/app/layout/evaluation/section/section-two-view/section-two-view.component.ts
+++ src/app/layout/evaluation/section/section-two-view/section-two-view.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, SimpleChanges, ViewChild } from '@angular/core';
+import { ModalDirective } from 'ngx-bootstrap';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'section-two-view',
@@ -6,11 +8,53 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./section-two-view.component.scss']
 })
 export class SectionTwoViewComponent implements OnInit {
+  
   @Input('data') data;
-  @Output('showModal') showModal:any=new EventEmitter<any>()
-  constructor() { }
+  @Input('productList') productList;
+  @ViewChild('childModal') childModal: ModalDirective;
+  @Output('showModal') showModal:any=new EventEmitter<any>();
+  @Output('productList') productForEmit:any=new EventEmitter<any>();
+  @Input('isEditable') isEditable :any;
+  selectedShop: any={}; 
+  ip=environment.ip;
+  products: any=[];
+  surveyId: number=0;
+  updatingMSL=false;
+  changeColor: boolean=false;
+  colorUpdateList:any=[];
+  availability: any;
+  
+
+  constructor() {
+    // var arr=router.url.split('/');
+    // this.surveyId=+arr[arr.length-1]
+    // console.log(this.surveyId)
+   }
+
+
 
   ngOnInit() {
   }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    
+    this.data=changes.data.currentValue;
+    // this.products=changes.productList.currentValue;
+    // if(this.products.length>0)
+    // this.availability=this.getAvailabilityCount(this.products);
+    // console.log('is editable',this.isEditable)
+    // this.getMSLCount(this.products)
+    
+  }
+  showChildModal(shop): void {
+    this.selectedShop=shop;
+    this.showModal.emit(this.selectedShop)
+    // this.childModal.show();
+  }
+ 
+  hideChildModal(): void {
+    // this.childModal.hide();
+  }
+
 
 }
