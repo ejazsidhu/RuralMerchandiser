3f3ee472a3fdf676551db3a7449b2470ae0757d7
Muhammad Awais <muhammad.awais@marksman.com.pk>
2019-08-07 10:47:01 +0500

    rte filer adde on merchandiser list page

--- src/app/layout/dashboard/inner-pages/merchandiser-list/merchandiser-list.component.ts
+++ src/app/layout/dashboard/inner-pages/merchandiser-list/merchandiser-list.component.ts
@@ -9,49 +9,79 @@ import * as moment from 'moment';
 })
 export class MerchandiserListComponent implements OnInit {
 
-  title="merchandiser List"
+  title = 'merchandiser List';
   minDate = new Date(2000, 0, 1);
-  maxDate:any = new Date();
-  startDate:any=new Date();
-  endDate=new Date();
-  loadingReportMessage=false;
-    merchandiserList: any=[];
+  maxDate: any = new Date();
+  startDate: any = new Date();
+  endDate = new Date();
+  loadingReportMessage = false;
+  merchandiserList: any = [];
   loading: boolean;
-    constructor(private httpService:DashboardService) { 
-  
-    this.maxDate.setDate(this.maxDate.getDate()-1);
-    this.startDate.setDate(this.startDate.getDate()-1)
-  
+  selectedRTE: any = {};
+  tempMerchandiserList: any = [];
+  rteList: any = [];
+
+  constructor(private httpService: DashboardService) {
+
+    this.maxDate.setDate(this.maxDate.getDate() - 1);
+    this.startDate.setDate(this.startDate.getDate() - 1);
+
     // this.startDate = moment(this.startDate).format('YYYY-MM-DD');
 
-  
-    }
-  
-    ngOnInit() {
-    this.getMerchandiserList(this.startDate);
-   
 
+  }
+
+  ngOnInit() {
+    this.getMerchandiserList(this.startDate);
+
+  }
+
+  getMerchandiserList(date) {
+    this.tempMerchandiserList = [];
+    this.rteList = [];
+    date = moment(date).format('YYYY-MM-DD');
+    const obj = {
+      evaluatorId: localStorage.getItem('user_id'),
+      startDate: date
+    };
+
+    this.httpService.getMerchandiserListForEvaluation(obj).subscribe((data: any) => {
+      // console.log('merchandiser list for evaluation',data);
+      if (data) {
+        this.tempMerchandiserList = data;
+        this.merchandiserList = data;
+        this.loading = false;
+        this.getRTEList();
+      }
+    });
+  }
+
+  modifyDate(date) {
+    return moment(date).format('YYYY-MM-DD');
+  }
+
+  getRTEList() {
+    const map = new Map();
+    for (const item of this.tempMerchandiserList) {
+      if (!map.has(item.rte_id)) {
+        map.set(item.rte_id, true);    // set any value to Map
+        this.rteList.push(item);
+      }
     }
-  
-    getMerchandiserList(date) {
-      date = moment(date).format('YYYY-MM-DD');
-      let obj = {
-        evaluatorId: localStorage.getItem('user_id'),
-        startDate: date
-      };
-  
-      this.httpService.getMerchandiserListForEvaluation(obj).subscribe((data: any) => {
-        // console.log('merchandiser list for evaluation',data);
-        if (data) {
-          this.merchandiserList = data;
-          this.loading = false;
-        }
-      });
+    this.rteList.sort((a, b) => (a.rte_name > b.rte_name) ? 1 : -1);
+  }
+
+  loadMerchandiserRTEWise() {
+    console.log(this.selectedRTE);
+    this.merchandiserList = [];
+    for (const item of this.tempMerchandiserList) {
+      if (this.selectedRTE.rte_id === item.rte_id) {
+        this.merchandiserList.push(item);
+      }
     }
-  
-    modifyDate(date){
-      return moment(date).format('YYYY-MM-DD');
-    }
-  
+    this.rteList.sort((a, b) => (a.rte_name > b.rte_name) ? 1 : -1);
+  }
+
+
 
 }
